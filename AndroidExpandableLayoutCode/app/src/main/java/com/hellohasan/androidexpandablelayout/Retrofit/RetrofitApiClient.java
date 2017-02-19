6fa69883c1c4f121f543f42048e7cee702735341
hasan_cse91 <hasan_cse91@yahoo.com>
2017-02-19 18:13:57 +0600

    UI done

---
+++ AndroidExpandableLayoutCode/app/src/main/java/com/hellohasan/androidexpandablelayout/Retrofit/RetrofitApiClient.java
@@ -0,0 +1,31 @@
+package com.hellohasan.androidexpandablelayout.Retrofit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitApiClient {
+ 
+        private static final String BASE_URL = "https://raw.githubusercontent.com/hasancse91/Android-Expandable-Layout/" + "master/related_files/";
+
+        private static Retrofit retrofit = null;
+ 
+        private static Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        private RetrofitApiClient() {} // So that nobody can create an object with constructor
+ 
+        public static synchronized Retrofit getClient() {
+            if (retrofit==null) {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+            }
+            return retrofit;
+        }
+ 
+}
